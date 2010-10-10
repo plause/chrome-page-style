0f6f06f57c8d955b336acafe20de3e4ad40060d2
Huazhou Wang <plause@gmail.com>
2010-10-11 00:27:40 +0800

    Add 'No Style' feature
        'No Style' disables all styles(linked stylesheets, embedded styles
        and inline styles) of the page.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,61 +1,116 @@
 (function () {
-  var alternate = /alternat(e|ive)/i;
+  var alternate = /alternate/i;
   var stylesheet = /stylesheet/i;
 
-  var detail = {"default": document.selectedStylesheetSet};
+  function isAlternate(link) {
+    return alternate.test($(link).attr("rel"));
+  }
 
-  function switchStylesheet(name) {
+  function isStylesheet(link) {
+    return stylesheet.test($(link).attr("rel"));
+  }
+
+  function isAlternateStylesheet(link) {
+    return isStylesheet(link) && isAlternate(link);
+  }
+
+  function isPersistentStylesheet(link) {
+    return isStylesheet(link) && !isAlternate(link) && !$(link).attr("title");
+  }
+
+  function isPageStyle(link) {
+    return isStylesheet(link) && $(link).attr("title");
+  }
+
+  function toggleLinkedStyles(toggle) {
     $("link").each(function () {
-      var link = $(this);
-      var title = link.attr("title");
-
-      if (stylesheet.test(link.attr("rel")) && title) {
-        link.attr("disabled", true).attr("disabled", title != name);
-      }
-    }).each(function () {
-      var link = $(this);
-      // enable persistent stylesheet
-      // a persistent stylesheet is a stylesheet without title
-      if (stylesheet.test(link.attr("rel")) && !link.attr("title")) {
-        link.attr("disabled", true).attr("disabled", false);
+      if (isStylesheet(this)) {
+        $(this).attr("disabled", true).attr("disabled", true);
       }
     });
   }
 
-  function refreshStylesheets() {
-    var flag = {};
-    var styles = detail.styles = [];
+  function toggleEmbeddedStyles(toggle) {
+    $("style").each(function () {
+      $(this.sheet).attr("disabled", toggle).attr("disabled", toggle);
+    });
+  }
 
-    $("link").each(function () {
-      var link = $(this);
-      var title = link.attr("title");
-
-      if (stylesheet.test(link.attr("rel")) && title) {
-        if (!flag[title] && (flag[title] = true)) {
-          styles.push(title);
+  function toggleInlineStyles(toggle) {
+    $("*").each(function () {
+      if (toggle) {
+        this.style["chrome-page-style"] = this.style.cssText;
+        this.style.cssText = "";
+      } else {
+        if (this.style["chrome-page-style"]) {
+          this.style.cssText = this.style["chrome-page-style"];
+          delete this.style["chrome-page-style"];
         }
       }
     });
+  }
 
-    detail.selected = document.selectedStylesheetSet;
+  /*
+   * disable all styles
+   */
+  function noStyles() {
+    toggleLinkedStyles(true);
+    toggleEmbeddedStyles(true);
+    toggleInlineStyles(true);
+  }
+
+  /*
+   * if name is specified, switch to it, otherwise do "baisc page style"
+   */
+  function switchStyle(name) {
+    $("link").each(function () {
+      var val = name ? $(this).attr("title") != name : isAlternate(this);
+
+      if (isStylesheet(this)) {
+        $(this).attr("disabled", val).attr("disabled", val);
+      }
+    }).each(function () {
+      if (isPersistentStylesheet(this)) {
+        $(this).attr("disabled", false).attr("disabled", false);
+      }
+    });
+
+    toggleEmbeddedStyles(false);
+    toggleInlineStyles(false);
+  }
+
+  /*
+   *
+   */
+  function refreshDetail() {
+    var flag = {};
+    var styles = [];
+
+    $("link").each(function () {
+      var title = $(this).attr("title");
+
+      if (isPageStyle(this) && !flag[title] && (flag[title] = true)) {
+        styles.push(title);
+      }
+    });
+
+    return {"styles": styles, "default": document.selectedStylesheetSet};
   }
 
   function onRequest(request, sender, sendResponse) {
     if (request.type == "detail") {
-      refreshStylesheets();
-      sendResponse(detail);
+      sendResponse(refreshDetail());
     } else if (request.type == "switch") {
-      switchStylesheet(request.style);
-      sendResponse({"result": true});
+      sendResponse(switchStyle(request.style));
+    } else if (request.type == "none") {
+      sendResponse(noStyles());
     }
   }
 
-  if (detail["default"] != null) {
-    switchStylesheet(detail["default"]);
-  }
-
-  refreshStylesheets();
-
-  chrome.extension.sendRequest(detail);
+  chrome.extension.sendRequest(refreshDetail());
   chrome.extension.onRequest.addListener(onRequest);
+
+  // fix chrome
+  noStyles();
+  switchStyle(document.selectedStylesheetSet);
 })(jQuery);
